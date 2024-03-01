f2ab86bb064325d752bda2f3c4f5ef47a730e14c
Alan <alanruizz0207@gmail.com>
2024-03-01 04:41:43 -0800

    I updated the Main.java file and created the matrix, added two files for Matrix Operations and to identify the Matrix Type

--- Main.java
+++ Main.java
@@ -1,6 +1,48 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
         //this is where we take input form user and output the matrix
-        System.out.println("hello World");
+        Scanner rows = new Scanner(System.in);
+        Scanner column = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
+      
+        /*
+            I have a feeling like this should be a loop until user says they are done using the "Calculator"
+        */
+
+        //here we get input for matrix data
+        System.out.println("Choose your matrix size: ");
+        System.out.print("Rows: "); 
+        int x = rows.nextInt();
+        System.out.print("Columns: ");
+        int y = rows.nextInt();
+        int matrix[][] = new int[x][y]; 
+       
+        
+        System.out.println("Great! Now going left to right, row to row, input the first number starting from a_ij where i = 0 and j = 0: "); 
+        
+        
+            for (int i = 0; i < x; i++) {
+                for (int j = 0; j < y; j++) {
+                    int input = userInput.nextInt();
+                    matrix[i][j] = input;
+                    
+                }
+            }
+        
+
+
+        //here we will output the matrix 
+
+        System.out.print("Here is your matrix: ");      
+        for (int i = 0; i < x; i++) {
+            System.out.println(""); 
+            for (int j = 0; j < y; j++) {
+                System.out.print(matrix[i][j] + " ");
+                
+            }
+        }
+        
     }
 }
--- Matrix.java
+++
@@ -1,9 +0,0 @@
-public class Matrix {
-    //this is where we put everthing together to form the Matrix
-
-    public void createMatrix(int row, int column)
-    { 
-        int matrix[][] = new int[row][column];
-
-    }
-}
