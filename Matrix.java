895b650b3175ed7b79e3dd9b8d2f1ef83fb2e5fd
Alan <alanruizz0207@gmail.com>
2024-03-01 03:37:20 -0800

    changes to Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,3 +1,9 @@
 public class Matrix {
     //this is where we put everthing together to form the Matrix
+
+    public void createMatrix(int row, int column)
+    { 
+        int matrix[][] = new int[row][column];
+
+    }
 }
